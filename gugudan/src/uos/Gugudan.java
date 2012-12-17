05ee9b25ee9cac925d5ca65331349fc60e715724
hjinbae <yellow2452@hanmail.net>
2012-12-17 12:53:25 +0900

    add source

---
+++ gugudan/src/uos/Gugudan.java
@@ -0,0 +1,18 @@
+package uos;
+
+public class Gugudan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			printDan(i);
+		}
+			
+		
+		public static void printDan (int i)
+			for (int j = 1; j <= 0; j++)
+				System.out.println("2" + " x 1 = 2");
+			}
+		System.out.println(i + " x " + j + " = " + i * j);
